84f089a7c22649aacb53aded819e46d05c7edc74
OTumanov <otumanov@gmail.com>
2023-06-15 09:09:08 +0300

    - add Comment entity - add CommentRepository

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -1,4 +1,36 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @NotNull
+    private Instant created;
 }
