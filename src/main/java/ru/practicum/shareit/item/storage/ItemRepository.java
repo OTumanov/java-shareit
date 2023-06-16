b5fa3990911a32a580396d0aac30b61059fa0371
OTumanov <otumanov@gmail.com>
2023-06-16 10:55:11 +0300

    - фикс Checkstyle

--- src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
@@ -6,11 +6,6 @@ import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
-//    @Query(" select i from Item i " +
-//            "where upper(i.name) like upper(concat('%', ?1, '%')) " +
-//            "   or upper(i.description) like upper(concat('%', ?1, '%'))")
-
     @Query("select it " +
             "from Item as it " +
             "where (upper(it.name) like upper('%'||?1||'%') " +
