e86e2dc4cff266ff0575e05cd2569eacb61c3a3c
OTumanov <otumanov@gmail.com>
2023-06-22 16:22:02 +0300

    - новая ветка add-item-requests - добавлена сущность ItemRequest

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,5 +1,35 @@
 package ru.practicum.shareit.request.model;
 
-public class ItemRequest {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "requests")
+@Data
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@SuppressWarnings("ALL")
+public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    private User requester;
+    @CreationTimestamp
+    private LocalDateTime created;
+    @Transient
+    private List<Item> items;
 }
